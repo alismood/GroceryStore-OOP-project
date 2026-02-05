6b68a137bc1214780da120f5e57c5c74b4e80f73
Ali Zhabaihan <252477@astanait.edu.kz>
2026-02-05 20:42:48 +0500

    week 8

--- src/database/ProductDAO.java
+++ src/database/ProductDAO.java
@@ -1,7 +1,8 @@
 package database;
 import model.FreshProduct;
 import model.Product;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.*;
 
 public class ProductDAO {
@@ -33,7 +34,7 @@ public class ProductDAO {
     }
 
     public void getAllFreshProduct(){
-        String sql = "SELECT * FROM FreshProduct";
+        String sql = "SELECT * FROM freshproduct";
 
         Connection connection = DatabaseConnection.getConnection();
 
@@ -69,4 +70,223 @@ public class ProductDAO {
             DatabaseConnection.closeConnection(connection);
         }
     }
+
+    public  boolean updateFreshProduct(FreshProduct product){
+        String sql = "UPDATE freshproduct SET name = ?, stockquantity = ?, price = ?, expirationdate = ?, storagetemperature = ? WHERE productid = ?";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return false;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+//            statement.setInt(1, product.getProductId());
+            statement.setString(1, product.getName());
+            statement.setInt(2, product.getStockQuantity());
+            statement.setDouble(3, product.getPrice());
+            statement.setString(4, product.getExpirationDate());
+            statement.setDouble(5, product.getStorageTemperature());
+            statement.setInt(6, product.getProductId());
+
+            int rowsUpdated = statement.executeUpdate();
+            statement.close();
+
+            if (rowsUpdated > 0){
+                System.out.println("Product updated: " + product.getName());
+                return true;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Update failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+
+        return false;
+    }
+
+    public FreshProduct getFreshProductById(int productId){
+        String sql = "SELECT * FROM freshproduct WHERE productid = ?";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return  null;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, productId);
+
+            ResultSet resultSet =statement.executeQuery();
+
+            if (resultSet.next()){
+                FreshProduct product = extractFreshProductFromResulSet(resultSet);
+
+                resultSet.close();
+                statement.close();
+
+                if (product != null) {
+                    System.out.println("Found Product with ID: " + productId);
+                }
+
+                return product;
+            }
+            System.out.println("No staff with ID: " + productId);
+
+            resultSet.close();
+            statement.close();
+
+        } catch (SQLException e){
+            System.out.println("Select by ID failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+        return null;
+    }
+
+    private FreshProduct extractFreshProductFromResulSet(ResultSet resultSet) throws SQLException {
+        int productId = resultSet.getInt("productid");
+        String name = resultSet.getString("name");
+        int stockQuantity = resultSet.getInt("stockquantity");
+        double price = resultSet.getDouble("price");
+        String date = resultSet.getString("expirationdate");
+        double temp = resultSet.getDouble("storagetemperature");
+
+        FreshProduct product = null;
+
+        product = new FreshProduct(productId, name, price , stockQuantity, date, temp);
+        return product;
+    }
+
+    public  boolean deleteFreshProduct(int productId){
+        String sql = "DELETE FROM freshproduct WHERE productid = ?";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return false;
+
+        try {
+            PreparedStatement statement =connection.prepareStatement(sql);
+            statement.setInt(1,productId);
+
+            int rowsDeleted = statement.executeUpdate();
+            statement.close();
+
+            if (rowsDeleted >0) {
+                System.out.println("Fresh Product deleted with ID: " + productId);
+                return true;
+            } else {
+                System.out.println("No Fresh Product was deleted with ID: " + productId);
+            }
+
+        } catch (SQLException e){
+            System.out.println("Delete failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+        return false;
+    }
+
+    public List<FreshProduct> searchByName(String name){
+        List<FreshProduct> freshProductList = new ArrayList<>();
+
+        String sql = "SELECT * FROM freshproduct WHERE name ILIKE ? ORDER BY name";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return freshProductList;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, "%" + name + "%");
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()){
+                FreshProduct freshProduct = extractFreshProductFromResulSet(resultSet);
+                if (freshProduct != null) {
+                    freshProductList.add(freshProduct);
+                }
+            }
+
+            resultSet.close();
+            statement.close();
+
+            System.out.println("Found " + freshProductList.size() + "fresh products");
+        } catch (SQLException e){
+            System.out.println("Search failed");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+        return freshProductList;
+    }
+
+    public List<FreshProduct> searchByPriceRange(double minPrice, double maxPrice){
+        List<FreshProduct> freshProductList = new ArrayList<>();
+
+        String sql = "SELECT * FROM freshproduct WHERE price BETWEEN ? AND ? ORDER BY price DESC";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if(connection==null) return freshProductList;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setDouble(1, minPrice);
+            statement.setDouble(2, maxPrice);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()){
+                FreshProduct freshProduct = extractFreshProductFromResulSet(resultSet);
+                if(freshProduct != null){
+                    freshProductList.add(freshProduct);
+                }
+            }
+
+            resultSet.close();
+            statement.close();
+
+            System.out.println("Found " + freshProductList.size() + " Fresh Products");
+
+        } catch (SQLException e) {
+            System.out.println("Search failed!");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+        return  freshProductList;
+    }
+
+    public List<FreshProduct> searchByMinPrice(Double minPrice){
+        List<FreshProduct> freshProductList = new ArrayList<>();
+
+        String sql = "SELECT * FROM freshproduct WHERE price >= ? ORDER BY price DESC";
+
+        Connection connection = DatabaseConnection.getConnection();
+        if (connection == null) return freshProductList;
+
+        try {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setDouble(1, minPrice);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()){
+                FreshProduct freshProduct = extractFreshProductFromResulSet(resultSet);
+                if (freshProduct != null) {
+                    freshProductList.add(freshProduct);
+                }
+            }
+
+            resultSet.close();
+            statement.close();
+
+        } catch (SQLException e){
+            System.out.println(" Error: search by min price in dao");
+            e.printStackTrace();
+        } finally {
+            DatabaseConnection.closeConnection(connection);
+        }
+        return freshProductList;
+    }
+
 }
--- src/menu/Storage.java
+++ src/menu/Storage.java
@@ -3,6 +3,7 @@ package menu;
 import database.ProductDAO;
 import model.*;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 import database.*;
@@ -17,12 +18,12 @@ public class Storage implements Menu {
         this.productDAO = new ProductDAO();
 
         //Test data
-        try {
-            inventory.add(new FreshProduct(101, "Milk", 750.0, 20, "2026-02-15", 4.0));
-            inventory.add(new PackagedProduct(201, "Cookie", 450.0, 50, "12345"));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+//        try {
+//            inventory.add(new FreshProduct(101, "Milk", 750.0, 20, "2026-02-15", 4.0));
+//            inventory.add(new PackagedProduct(201, "Cookie", 450.0, 50, "12345"));
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
     }
 // displaymenu
     @Override
@@ -30,11 +31,16 @@ public class Storage implements Menu {
         System.out.println("\n========================================");
         System.out.println("      GROCERY STORE INVENTORY SYSTEM");
         System.out.println("========================================");
-        System.out.println("1. Add Fresh Product(week 7-8 assignment");
-        System.out.println("2. Add Packaged Product");
-        System.out.println("3. View All Inventory");
-        System.out.println("4. View Fresh Products Only(week 7-8 assignment");
+        System.out.println("1. Add Fresh Product(Insert week 7)");
+        System.out.println("2. Add Packaged Product(week 6)");
+        System.out.println("3. View All Inventory(week6)");
+        System.out.println("4. View Fresh Products Only(Select week 7)");
         System.out.println("5. Run Quality Checks (Polymorphism)");
+        System.out.println("6. Update Fresh Product(Update week 8)");
+        System.out.println("7. Delete Fresh Product by ID(Delete week 8)");
+        System.out.println("8. Search by name(Search week 8)");
+        System.out.println("9. Search by price range(search week 8)");
+        System.out.println("10. Search by high price(Search week 8)");
         System.out.println("0. Exit");
         System.out.println("========================================");
     }
@@ -57,6 +63,11 @@ public class Storage implements Menu {
                     case 3: viewAllInventory(); break;
                     case 4: viewFreshProductsOnly(); break;
                     case 5: demonstratePolymorphism(); break;
+                    case 6: updateFreshProduct(); break;
+                    case 7: deleteFreshProduct(); break;
+                    case 8: searchByName(); break;
+                    case 9: searchBYPriceRange(); break;
+                    case 10: searchBYPriceRange(); break;
                     case 0: running = false; break;
                     default: System.out.println("Invalid choice");
                 }
@@ -129,16 +140,16 @@ public class Storage implements Menu {
     }
 
     private void viewFreshProductsOnly() {
-        System.out.println("\n--- FRESH PRODUCE FILTER ---");
-        boolean found = false;
-        for (Product p : inventory) {
-            if (p instanceof FreshProduct) {
-                System.out.println(p);
-                found = true;
+//        System.out.println("\n--- FRESH PRODUCE FILTER ---");
+//        boolean found = false;
+//        for (Product p : inventory) {
+//            if (p instanceof FreshProduct) {
+//                System.out.println(p);
+//                found = true;
                 productDAO.getAllFreshProduct();
-            }
-        }
-        if (!found) System.out.println("No fresh products found.");
+//            }
+//        }
+//        if (!found) System.out.println("No fresh products found.");
     }
 
     private void demonstratePolymorphism() {
@@ -151,5 +162,156 @@ public class Storage implements Menu {
             System.out.println("Checking " + p.getName() + ": " + p.toString());
         }
         System.out.println("Quality check complete.");
+    }
+
+    private  void updateFreshProduct(){
+        System.out.println("Enter freshproductID to update");
+        int productid = scanner.nextInt();
+        scanner.nextLine();
+
+        FreshProduct existingFreshProduct = productDAO.getFreshProductById(productid);
+
+        if (existingFreshProduct == null){
+            System.out.println("No fresh Product with ID: " + productid);
+            return;
+        }
+
+        System.out.println("| Current Info:");
+        System.out.println("|" + existingFreshProduct.toString());
+        System.out.println("|______________________________________________|");
+
+        System.out.println("\n┌─ ENTER NEW VALUES ─────────────────────┐");
+        System.out.println("│ (Press Enter to keep current value)   │");
+
+//        System.out.println("| new ID [" + existingFreshProduct.getProductId() + "]: ");
+//        String productIdInput = scanner.nextLine();
+//        int newProductId = productIdInput.trim().isEmpty() ?
+//                existingFreshProduct.getStockQuantity() : Integer.parseInt(productIdInput);
+
+        System.out.println("| new name [" + existingFreshProduct.getName() + "]: ");
+        String newName = scanner.nextLine();
+        if (newName.trim().isEmpty()){
+            newName = existingFreshProduct.getName();
+        }
+
+        System.out.println("| new Stock quantity [" + existingFreshProduct.getStockQuantity() + "]: ");
+        String stockQuantityInput = scanner.nextLine();
+        int newStockQuantity = stockQuantityInput.trim().isEmpty() ?
+                existingFreshProduct.getStockQuantity() : Integer.parseInt(stockQuantityInput);
+
+        System.out.println("| New Price [" + existingFreshProduct.getPrice() + "]: ");
+        String priceInput = scanner.nextLine();
+        double newPrice = priceInput.trim().isEmpty() ?
+                existingFreshProduct.getPrice() : Double.parseDouble(priceInput);
+
+        System.out.println("New expiration date [" + existingFreshProduct.getExpirationDate() + "]: ");
+        String newExpirationDate = scanner.nextLine();
+        if (newExpirationDate.trim().isEmpty()){
+            newExpirationDate = existingFreshProduct.getExpirationDate();
+        }
+
+        System.out.println("New Sotage Temperature [" + existingFreshProduct.getStorageTemperature() + "]: ");
+        String storageTemperatureInput = scanner.nextLine();
+        double newStorageTemperature = storageTemperatureInput.trim().isEmpty() ?
+                existingFreshProduct.getStorageTemperature() : Double.parseDouble(storageTemperatureInput);
+
+        FreshProduct updatedFreshProduct = new FreshProduct(productid, newName, newPrice, newStockQuantity, newExpirationDate, newStorageTemperature);
+        productDAO.updateFreshProduct(updatedFreshProduct);
+
+
+        System.out.println("└────────────────────────────────────────┘");
+
+    }
+
+    private void deleteFreshProduct(){
+        System.out.println("Enter Fresh Product ID to delete: ");
+        int productId = scanner.nextInt();
+        scanner.nextLine();
+
+        FreshProduct product = productDAO.getFreshProductById(productId);
+
+        if (product == null) {
+            System.out.println("No Fresh Product deleted");
+            return;
+        }
+
+        System.out.println("Fresh Product to delete: ");
+        System.out.println(product.getProductId());
+
+        System.out.println("Are you sure?(yes/no): ");
+        String confirmation = scanner.nextLine();
+
+        if (confirmation.equalsIgnoreCase("yes")){
+            productDAO.deleteFreshProduct(productId);
+        } else {
+            System.out.println("Deletion cancelled");
+        }
+    }
+
+    private void searchByName(){
+        System.out.println("\n -------Search By Name -----------------");
+        System.out.println("Enter name to search: ");
+        String name = scanner.nextLine();
+        System.out.println("-------------------------------------------");
+
+        List<FreshProduct> freshProductList = productDAO.searchByName(name);
+
+        displaySearchResults(freshProductList, "Search: '" + name + "'");
+    }
+
+    private void displaySearchResults(List<FreshProduct> freshProductList, String criteria){
+        System.out.println("\n ---------------------------------");
+        System.out.println("       Search REsults");
+        System.out.println("-------------------------------------");
+        System.out.println("Criteria: " + criteria);
+        System.out.println("-------------------------------------");
+
+        if (freshProductList.isEmpty()){
+            System.out.println("No fresh Product matching criteria");
+        } else {
+            for(int i = 0; i < freshProductList.size(); i++){
+                FreshProduct s = freshProductList.get(i);
+                System.out.println((i + 1) + ". ");
+                System.out.println(s.toString());
+            }
+            System.out.println("Total results: " + freshProductList.size());
+        }
+    }
+
+    private void searchBYPriceRange(){
+        try {
+            System.out.println("\n┌─ SEARCH BY PRIce RANGE ───────────────┐");
+            System.out.print("│ Enter minimum price: ");
+            double minPrice = scanner.nextDouble();
+
+            System.out.println("Max price: ");
+            double maxPrice = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("________________________________________________");
+            List<FreshProduct> freshProductList = productDAO.searchByPriceRange(minPrice, maxPrice);
+
+            displaySearchResults(freshProductList, "Price: " + minPrice + "--" + maxPrice + "KZT");
+        } catch (java.util.InputMismatchException e){
+            System.out.println("Error: Invalid Number!(Searchbyprice");
+            scanner.nextLine();
+
+        }
+    }
+
+    private void searchHighPriceFreshProduct(){
+        try {
+            System.out.println("\n -----HIGH PRICE FRESH PRODUCT");
+            System.out.println(" ENter min price: ");
+            double minPrice = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("_________________________________");
+
+            List<FreshProduct> freshProductList = productDAO.searchByMinPrice(minPrice);
+
+            displaySearchResults(freshProductList, "price >=" + minPrice + "KZT");
+        } catch (java.util.InputMismatchException e) {
+            System.out.println("Error: highprice sorage");
+            scanner.nextLine();
+        }
     }
 }
